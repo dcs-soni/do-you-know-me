0bf4ec01352ce617f6dc40ac04ae903ee2ba0397
Divyanshu Soni <52232900+dcs-soni@users.noreply.github.com>
2021-07-21 11:59:47 +0000

    refactor: modify some code and statements

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var readlineSync = require("readline-sync");
 var score = 0;
 
 var questions = [{
-  question: "\nDuggu is my real name but do you really know my real name? ",
+  question: "\nDuggu is my pet name 😁 Do you know my real name? ",
   answer: "Divyanshu"
 }, 
 {
@@ -19,33 +19,15 @@ var questions = [{
 }];
 
 
-function start() {
-  console.log("------------------------------");
-  var userName = readlineSync.question("What's your name? ");
-
-  console.log("Welcome " + userName + " Let's play a game!");
-  console.log("------------------------------");
-}
-
-//The function will compare the original answer and answer given by player
-function play(question, answer){
-  var userAnswer = readlineSync.question(question);
-
-  if(userAnswer.toLowerCase() === answer.toLowerCase()){
-    correct();
-  }else{
-    wrong(answer); 
-  }
-}
-
-
+//If ans is correct this func is executed
 function correct() {
-  console.log("Dammn, you are right :-");
+  console.log("Dammn, you are right!!");
   score += 1;
   console.log("\nCurrent Score: ", score, "\n");
   console.log("-------------------");
 }
 
+//If ans is wrong this function is executed
 function wrong(answer) {
   console.log("Ohh snap!! You did a mistake");
   console.log("Correct ans is ", answer);
@@ -60,19 +42,43 @@ function wrong(answer) {
   console.log("-------------------");
 }
 
+
+
+//The function will compare the original answer and answer given by player
+function play(question, answer){
+  var userAnswer = readlineSync.question(question);
+
+  if(userAnswer.toLowerCase() === answer.toLowerCase()){
+    correct();
+  }else{
+    wrong(answer); 
+  }
+}
+
 //This will loop through the questions
 function game() {
   for (var i=0; i<questions.length; i++) {
     var currentQuestion = questions[i];
-    console.log(currentQuestion)
+    // console.log(currentQuestion)
     play(currentQuestion.question, currentQuestion.answer);
   }
 }
 
 
-//Starting point
+
+function start() {
+  console.log("------------------------------");
+  var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome " + userName + " Let's play a game!");
+  console.log("------------------------------");
+
+  game();
+}
+
+
+//Start the game
 start();
-game();
 
 
 console.log("Final Score: ", score);
